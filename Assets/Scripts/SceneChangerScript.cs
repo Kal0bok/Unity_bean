ce43dbd0a2544c43811b73b9c77c3210fbbd0a4e
Bogdans <bogdans.cumaks@sk.lvt.lv>
2026-02-03 20:04:56 +0300

    Izveidoju visas scenas

--- Assets/Scripts/SceneChangerScript.cs
+++ Assets/Scripts/SceneChangerScript.cs
@@ -1,27 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChangerScript : MonoBehaviour
 {
-    public void LoadWithDelay(string sceneName)
+    public int sceneNumber;
+    public void Transition()
     {
-        StartCoroutine(LoadSceneAfterDelay(sceneName));
-    }
-
-    private IEnumerator LoadSceneAfterDelay(string sceneName)
-    {
-        yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-    }
-
-    public void QuitApplication()
-    {
-       /* if(UnityEditor.EditorApplication.isPlaying)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        
-        } else*/
-            Application.Quit();
+        SceneManager.LoadScene(sceneNumber);
     }
 }
